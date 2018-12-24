946d5c8ad9640507af72d003ed13a104dda29f27
Joao Tavares <joao.tavares@cubos.io>
2018-12-24 12:08:07 -0300

    ADD: Client webpage

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const graphqlHTTP = require('express-graphql');
 const schema = require('../server/schema/schema.js');
 const mongoose = require('mongoose');
 
+const path = require('path');
+
 const app = express();
 
 mongoose.connect('mongodb://tavares:joao.1234@ds151463.mlab.com:51463/gql-learning');
 mongoose.connection.once('open', () => {
     console.log('Connected to database');
 });
+
+// app.get('/', (req, res) => res.send('API working at PORT:4000/graphql'))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + `/welcomePage.html`)))
 
 //middleware
 app.use('/graphql', graphqlHTTP({

4d25ab96a9b5dc1fe1504bf02b569db306da6273
Joao Tavares <joao.tavares@cubos.io>
2018-12-24 18:32:26 -0300

    CREATE: Audio Schema at MongoDB

---
+++ models/audio.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const audioSchema = new Schema({
+    audioBuffer: Buffer,
+    createdAt: Date,
+    updatedAt: Date,
+    createdBy: String,
+    updatedBy: String,
+    userId: String,
+});
+
+module.exports = mongoose.model('Audio', audioSchema);
